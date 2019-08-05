20c5d4b38128c6ebebac5fc9f8fa3b90fad24e6f
Silas Groskreutz <silasgroskreutz@gmail.com>
2019-08-05 16:12:51 -0500

    delete button working

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-/* $('#btn1').click(function(){
-    alert('Button Clicked!');
-}); */
-
+/* this function changes if an item is checked or not
+    it uses closest with find to toggle on/off the checked class 
+*/
 $('.shopping-item-toggle').click(function(e){
     $(e.target).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
 });
 
-/*$("#js-shopping-list-form").on('submit', function(e){
-    let newItem = $('shopping-list-entry').val();
-    $(".shopping-list").append();
-
+// This is the add item to list function
+$("#js-shopping-list-form").on('submit', function(e){
     e.preventDefault();
-}); */
+    let new_item = $('#shopping-list-entry').val();
+    $(".shopping-list").append('<li>'+new_item+'</li>');
+});
+
+// this is the "delete" functionality button
+$('.shopping-item-delete').click(function(e){
+    $(e.target).closest("li").remove();
+});
