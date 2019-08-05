dc5430b97729c40c93a389cfb0e38a5ecb334c43
Silas Groskreutz <silasgroskreutz@gmail.com>
2019-08-05 17:36:57 -0500

    Add works but not able to get buttons working

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-/* this function changes if an item is checked or not
-    it uses closest with find to toggle on/off the checked class 
-*/
-$('.shopping-item-toggle').click(function(e){
+// This is the add item to list function
+$("#js-shopping-list-form").on('submit', function(e){
+    //prevents form submission
+    e.preventDefault();
+    //stores user input for use
+    let new_item = $('#shopping-list-entry').val();
+    //appends input to the ul
+    $(".shopping-list").append('<li><span class="shopping-item">'+new_item+'</span></li>');
+    //clears the input field
+    $('#shopping-list-entry').val("");
+});
+
+/* This is the add item to list function -- THIS ADDS BUTTONS BUT THEY DO NOT WORK
+$("#js-shopping-list-form").on('submit', function(e){
+    //prevents form submission
+    e.preventDefault();
+    //stores user input for use
+    let new_item = $('#shopping-list-entry').val();
+    //appends input to the ul
+    $(".shopping-list").append('<li><span class="shopping-item">'+new_item+'</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    //clears the input field
+    $('#shopping-list-entry').val("");
+}); */
+
+// this function toggles if an item is checked or not
+$('.shopping-item-toggle').on('click', function(e){
     $(e.target).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
 });
 
-// This is the add item to list function
-$("#js-shopping-list-form").on('submit', function(e){
-    e.preventDefault();
-    let new_item = $('#shopping-list-entry').val();
-    $(".shopping-list").append('<li>'+new_item+'</li>');
-});
-
 // this is the "delete" functionality button
-$('.shopping-item-delete').click(function(e){
-    $(e.target).closest("li").remove();
+$('.shopping-item-delete').on('click', function(){
+    $(this).closest("li").remove();
 });
